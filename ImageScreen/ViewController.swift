bb627ef5efaf59d09582b1a2c07e6ef502a74de7
Kirill <syskirill.com>
2024-07-11 21:57:30 +0300

    Initial Commit

---
+++ ImageScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageScreen
+//
+//  Created by Кирилл Сысоев on 11.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
