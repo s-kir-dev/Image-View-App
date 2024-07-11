6d105ac1dc80b88f429d521199985f8f03fcf3e2
Kirill <syskirill.com>
2024-07-11 22:29:33 +0300

    testing the mistake

--- ImageScreen/ViewController.swift
+++ ImageScreen/ViewController.swift
@@ -11,9 +11,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        hideAllImageView(true)
+        
     }
 
-
+    func hideAllImageView(_ show : Bool) {
+        imageView1.isHidden = show
+        imageView2.isHidden = show
+        imageView3.isHidden = show
+    }
+    
+    func hideImageView(_ image : UIImageView) {
+        image.isHidden = true
+    }
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    @IBAction func showAllTapped(_ sender: Any) {
+        
+        hideAllImageView(false)
+        
+    }
+    
+    @IBAction func hideImageView1Tapped(_ sender: Any) {
+        hideImageView(imageView2)
+    }
+    
+    @IBAction func hideImageView2Tapped(_ sender: Any) {
+        hideImageView(imageView2)
+    }
+    
+    @IBAction func hideImageView3Tapped(_ sender: Any) {
+        hideImageView(imageView3)
+    }
+    
 }
 
